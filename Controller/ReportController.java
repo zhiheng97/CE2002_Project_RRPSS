7781236d71856c06f97b28473056ee913a696d03
brianleect <63389110+brianleect@users.noreply.github.com>
2021-11-04 03:14:51 +0800

    [Test Case] Account for non-sequential edge case

--- Controller/ReportController.java
+++ Controller/ReportController.java
@@ -25,10 +25,6 @@ public class ReportController {
 	 * @param invoice // Order to be added to update Report
 	 */
 	public void addInvoice(Order invoice) {
-		// TODO: Not sure if we are considering cases where completed orders do not come
-		// sequentially for date?
-		// Solution: Might want to consider creating a linkedhashmap with date as key?
-		// Is it relevant though? Will a restaurant be taking orders around midnight?
 		Report currentReport;
 		String orderDate = invoice.getTimeStamp().split(" ")[0]; // Get new date from order
 		int report_size = reports.size();
@@ -71,11 +67,12 @@ public class ReportController {
 		// TODO: Consider GST as output field? Just add 7% tax to total price?
 
 		double salesRevenue = 0;
+		int reports_size = reports.size();
 
 		if (reports.size() == 0) {
 			System.out.println("No reports exist currently. Exiting");
 			return;
-		} else if (!byMonth)
+		} else if (!byMonth || reports_size == 1)
 			// Prints latest daily report if byMonth not specified
 			reports.get(reports.size() - 1).print();
 		else {
@@ -83,51 +80,53 @@ public class ReportController {
 			LinkedHashMap<String, Integer> item_map = new LinkedHashMap<String, Integer>();
 			LinkedHashMap<String, Integer> promo_map = new LinkedHashMap<String, Integer>();
 			// Get last 30 reports
-			int reports_size = reports.size();
+
+			int start_index = reports_size - 1;
+			int end_index;
 			Report tmpReport;
 
-			// Case 1: Report < 30
 			if (reports_size < 30) {
-				String start_date = reports.get(0).getDate();
-				String end_date = reports.get(reports_size - 1).getDate();
+				end_index = reports_size - 1; // End will always point to latest date
+				start_index = 0;
+			} else {
+				end_index = reports_size - 1;
+				start_index = end_index - 30 + 1;
+			}
 
-				for (int i = 0; i < reports_size; i++) {
-					tmpReport = reports.get(i);
-					salesRevenue += tmpReport.getSalesRevenue(); // Update sales revenue
-					int count;
-					String item_name;
-					LinkedHashMap<String, Integer> tmpReportItems = tmpReport.getItemMap();
-					for (Map.Entry<String, Integer> e : tmpReportItems.entrySet()) {
-						item_name = e.getKey();
-						count = item_map.containsKey(item_name) ? item_map.get(item_name) : 0;
-						item_map.put(item_name, count + e.getValue());
-					}
+			String start_date = reports.get(start_index).getDate();
+			String end_date = reports.get(end_index).getDate();
 
-					String promo_name;
-					LinkedHashMap<String, Integer> tmpReportPromos = tmpReport.getPromoMap();
-					for (Map.Entry<String, Integer> e : tmpReportPromos.entrySet()) {
-						promo_name = e.getKey();
-						count = promo_map.containsKey(promo_name) ? promo_map.get(promo_name) : 0;
-						promo_map.put(promo_name, count + e.getValue());
-					}
-
+			for (int i = 0; i < end_index + 1; i++) {
+				tmpReport = reports.get(i);
+				salesRevenue += tmpReport.getSalesRevenue(); // Update sales revenue
+				int count;
+				String item_name;
+				LinkedHashMap<String, Integer> tmpReportItems = tmpReport.getItemMap();
+				for (Map.Entry<String, Integer> e : tmpReportItems.entrySet()) {
+					item_name = e.getKey();
+					count = item_map.containsKey(item_name) ? item_map.get(item_name) : 0;
+					item_map.put(item_name, count + e.getValue());
 				}
-				System.out.println("\n\n(Monthly Report " + "(" + reports.size() + ") ) Start: " + start_date
-						+ " / End: " + end_date);
-				System.out.println("Monthly Sales: " + salesRevenue);
-				System.out.println("\nItems:");
-				for (Map.Entry<String, Integer> e : item_map.entrySet())
-					System.out.println("Item: " + e.getKey() + " Quantity: " + e.getValue());
 
-				System.out.println("\nPromos:");
-				for (Map.Entry<String, Integer> e : promo_map.entrySet())
-					System.out.println("Promo: " + e.getKey() + " Quantity: " + e.getValue());
-			}
-			// Case 2: > 30 reports
-			else if (reports_size > 30) {
-				return;
-			}
+				String promo_name;
+				LinkedHashMap<String, Integer> tmpReportPromos = tmpReport.getPromoMap();
+				for (Map.Entry<String, Integer> e : tmpReportPromos.entrySet()) {
+					promo_name = e.getKey();
+					count = promo_map.containsKey(promo_name) ? promo_map.get(promo_name) : 0;
+					promo_map.put(promo_name, count + e.getValue());
+				}
 
+			}
+			System.out.println(
+					"\n\n(Monthly Report " + "(" + reports.size() + ") ) Start: " + start_date + " / End: " + end_date);
+			System.out.println("Monthly Sales: " + salesRevenue);
+			System.out.println("\nItems:");
+			for (Map.Entry<String, Integer> e : item_map.entrySet())
+				System.out.println("Item: " + e.getKey() + " Quantity: " + e.getValue());
+
+			System.out.println("\nPromos:");
+			for (Map.Entry<String, Integer> e : promo_map.entrySet())
+				System.out.println("Promo: " + e.getKey() + " Quantity: " + e.getValue());
 		}
 	}
 }
